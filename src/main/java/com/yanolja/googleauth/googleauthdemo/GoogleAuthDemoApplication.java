a1a86e56e00300685fa404707bb220cc1e2fe8e2
Sooyoung Park <sooyoungpark@Sooyoungui-MacBook-Pro.local>
2017-05-23 17:48:46 +0900

    first commit

---
+++ src/main/java/com/yanolja/googleauth/googleauthdemo/GoogleAuthDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yanolja.googleauth.googleauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yanolja/googleauth/googleauthdemo/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.yanolja.googleauth.googleauthdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Created by sooyoungpark on 2017. 5. 23..
+ */
+@Controller
+public class TestController {
+
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+}
